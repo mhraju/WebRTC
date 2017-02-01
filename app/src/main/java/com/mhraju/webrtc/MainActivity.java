8fe22a5baca728438ab0b6f709def232b6160165
Maksudul Hasan Raju <rajuaiub1993@gmail.com>
2017-02-01 14:01:16 +0600

    Initial commit

---
+++ app/src/main/java/com/mhraju/webrtc/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mhraju.webrtc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://appear.in/mhraju");
+
+        
+    }
+}
