4f803eeef1d96c4838b9b3e33537f9624273d7e0
Maksudul Hasan Raju <rajuaiub1993@gmail.com>
2017-02-02 17:09:21 +0600

    few changes

--- app/src/main/java/com/mhraju/webrtc/MainActivity.java
+++ app/src/main/java/com/mhraju/webrtc/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private AdView mBannerAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
 
-
+        mBannerAd = (AdView) findViewById(R.id.banner_AdView);
+        
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://appear.in/mhraju");
+        mWebView.loadUrl("https://appear.in/");
 
-        
+
+        showBannerAd();
+
+    }
+
+    private void showBannerAd() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("ca-app-pub-6008099628983320~8146629099")
+                .build();
+        mBannerAd.loadAd(adRequest);
     }
 }
