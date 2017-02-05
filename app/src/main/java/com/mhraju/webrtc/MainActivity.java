ea82e771e14dfcc2a24ae6fe87179ed2a95396b6
Maksudul Hasan Raju <rajuaiub1993@gmail.com>
2017-02-05 18:43:08 +0600

    few changeshgyhuj

--- app/src/main/java/com/mhraju/webrtc/MainActivity.java
+++ app/src/main/java/com/mhraju/webrtc/MainActivity.java
@@ -1,7 +1,14 @@
 package com.mhraju.webrtc;
 
+import android.content.ComponentName;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -12,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
     private AdView mBannerAd;
+    final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
+    final String websiteURL = "http://viralandroid.com/";
+    final String googleURL = "http://google.com/";
+
+    CustomTabsClient mCustomTabsClient;
+    CustomTabsSession mCustomTabsSession;
+    CustomTabsServiceConnection mCustomTabsServiceConnection;
+    CustomTabsIntent mCustomTabsIntent;
+
+    // website to be visited
+    static final String SITE_URL = "http://androidcss.com/";
+
+    // Define variables for custom tabs and its builder
+    CustomTabsIntent customTabsIntent;
+    CustomTabsIntent.Builder intentBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +43,16 @@ public class MainActivity extends AppCompatActivity {
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
 
         mBannerAd = (AdView) findViewById(R.id.banner_AdView);
-        
+
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.loadUrl("https://appear.in/");
 
-
-        showBannerAd();
+       showBannerAd();
 
     }
+
+
 
     private void showBannerAd() {
         AdRequest adRequest = new AdRequest.Builder()
@@ -37,4 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         mBannerAd.loadAd(adRequest);
     }
+
+
+
 }
