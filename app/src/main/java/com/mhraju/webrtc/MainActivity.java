e225b9f4730469a27fb755e4c5d0eea8b95feaf5
Maksudul Hasan Raju <rajuaiub1993@gmail.com>
2017-02-06 18:29:41 +0600

    few

--- app/src/main/java/com/mhraju/webrtc/MainActivity.java
+++ app/src/main/java/com/mhraju/webrtc/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.customtabs.CustomTabsSession;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     CustomTabsIntent mCustomTabsIntent;
 
     // website to be visited
-    static final String SITE_URL = "http://androidcss.com/";
+    static final String SITE_URL = "https://appear.in/";
 
     // Define variables for custom tabs and its builder
     CustomTabsIntent customTabsIntent;
@@ -40,7 +42,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+/*
+
+        // Initialize intentBuilder
+        intentBuilder = new CustomTabsIntent.Builder();
+
+        // Set toolbar(tab) color of your chrome browser
+        intentBuilder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        // Define entry and exit animation
+        intentBuilder.setExitAnimations(this, R.anim.right_to_left_end, R.anim.left_to_right_end);
+        intentBuilder.setStartAnimations(this, R.anim.left_to_right_start, R.anim.right_to_left_start);
+       // intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        // build it by setting up all
+        customTabsIntent = intentBuilder.build();
+
+    }
+
+    // triggers when button clicked
+    public void ButtonClicked(View arg0) {
+
+        // go to website defined above
+        customTabsIntent.launchUrl(this, Uri.parse(SITE_URL));
+
+    }
+
+}
+*/
+
+
+
+
+
+
+
+
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
 
         mBannerAd = (AdView) findViewById(R.id.banner_AdView);
 
